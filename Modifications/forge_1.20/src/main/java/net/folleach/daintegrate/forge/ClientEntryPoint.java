ee1f5cba7539302fd9d4a0c82110522f756d5b34
Folleach <andrey@folleach.ru>
2023-11-06 19:46:24 +0500

    it's finally build forge and fabric together

--- Modifications/forge_1.20/src/main/java/net/folleach/daintegrate/forge/ClientEntryPoint.java
+++ Modifications/forge_1.20/src/main/java/net/folleach/daintegrate/forge/ClientEntryPoint.java
@@ -93,8 +93,6 @@ public class ClientEntryPoint {
             LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
 
         LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
--- Modifications/forge_1.20/src/main/java/net/folleach/daintegrate/forge/Config.java
+++ Modifications/forge_1.20/src/main/java/net/folleach/daintegrate/forge/Config.java
@@ -31,11 +31,6 @@ public class Config {
             .comment("What you want the introduction message to be for the magic number")
             .define("magicNumberIntroduction", "The magic number is... ");
 
-    // a list of strings that are treated as resource locations for items
-    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
-
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
     public static boolean logDirtBlock;
@@ -53,9 +48,5 @@ public class Config {
         magicNumber = MAGIC_NUMBER.get();
         magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
 
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
-                .collect(Collectors.toSet());
     }
 }
